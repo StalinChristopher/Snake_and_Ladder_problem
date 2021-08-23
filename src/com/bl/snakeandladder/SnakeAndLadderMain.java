1d7caff72551ef65482551f3c15cffcd05f83a02
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-08-23 22:58:08 +0530

    added logic for repeating process till the player wins the game

--- src/com/bl/snakeandladder/SnakeAndLadderMain.java
+++ src/com/bl/snakeandladder/SnakeAndLadderMain.java
@@ -5,29 +5,43 @@ public class SnakeAndLadderMain {
 	public static void main(String[] args) {
 		
 		int position = 0;
-		int diceNum;
+		int dieNum;
 		int option;
 		
 		System.out.println("Welcome to Snake and Ladder program");
-		System.out.printf("\nPlayer 1 is in position : %d",position);
 		
-		diceNum=(int)Math.floor(Math.random()*((6-1)+1)+1);
-		
-		System.out.printf("\nDice number : %d\n",diceNum);
-		
-		option = (int)Math.floor(Math.random()*((3-1)+1)+1);
-		switch(option) {
-		case 1: 
-			position+=diceNum;
-			System.out.printf("\nOption Ladder : Player has inclined by %d and moved into position %d\n",diceNum,position);
-			break;
-		case 2:
-			position-=diceNum;
-			System.out.printf("\nOption Snake : Player has declined by %d and moved into position %d\n",diceNum,position);
-			break;
-		case 3:
-			System.out.printf("\nOption No Play : Player stays in the same position %d\n",position);
+		while(position<=100) {
+//			if(position<0)
+//				position=0;
+			System.out.printf("\nPlayer 1 current position : %d",position);
+			
+			dieNum=(int)Math.floor(Math.random()*((6-1)+1)+1);
+			
+			System.out.printf("\nDie number : %d",dieNum);
+			
+			option = (int)Math.floor(Math.random()*((3-1)+1)+1);
+			switch(option) {
+			case 1: 
+				position+=dieNum;
+				System.out.printf("\nOption Ladder\nPlayer has inclined by %d \nNew position: %d\n",dieNum,position);
+				break;
+			case 2:
+				position-=dieNum;
+				if(position<0) {
+					position=0;
+					System.out.printf("\nOption Snake\nPlayer has declined by %d \nPosition<0 \nNew position: %d\n",dieNum,position);
+					break;
+				}
+				System.out.printf("\nOption Snake \nPlayer has declined by %d \nNew position: %d\n",dieNum,position);
+				break;
+			case 3:
+				System.out.printf("\nOption No Play\nPlayer stays in the same position \nNew Position: %d\n",position);
+			}
 		}
+		System.out.printf("\nCongratulations!!!! \nYou have won the game");
+		
+		
+		
 	}
 
 }
