eed0d315de5eca6bb29c8850b91755d51a5d88f6
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-08-23 20:14:39 +0530

    Added logic to display dice number between 1 and 6

--- src/com/bl/snakeandladder/SnakeAndLadderMain.java
+++ src/com/bl/snakeandladder/SnakeAndLadderMain.java
@@ -5,9 +5,14 @@ public class SnakeAndLadderMain {
 	public static void main(String[] args) {
 		
 		int position = 0;
+		int diceNum;
 		
 		System.out.println("Welcome to Snake and Ladder program");
 		System.out.printf("\nPlayer 1 is in position : %d",position);
+		
+		diceNum=(int)Math.floor(Math.random()*((6-1)+1)+1);
+		
+		System.out.printf("\nDice number : %d",diceNum);
 	}
 
 }
