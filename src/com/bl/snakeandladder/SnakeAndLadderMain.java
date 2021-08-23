0631f95b6b2310db6df39fd4932e6155c2d14d83
Stalin CD <stalincd@120AMBBLR-Samyad-R-Jain.local>
2021-08-23 19:52:14 +0530

    Initialized the java project displaying welcome message

---
+++ src/com/bl/snakeandladder/SnakeAndLadderMain.java
@@ -0,0 +1,10 @@
+package com.bl.snakeandladder;
+
+public class SnakeAndLadderMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder program");
+
+	}
+
+}
