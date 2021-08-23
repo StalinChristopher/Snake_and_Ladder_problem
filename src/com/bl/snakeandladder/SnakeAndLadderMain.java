196459e82f45799e8da3e002338f95da39c5adef
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-08-23 23:24:07 +0530

    Addedlogic where process is repeated until player reaches to exact position 100

--- src/com/bl/snakeandladder/SnakeAndLadderMain.java
+++ src/com/bl/snakeandladder/SnakeAndLadderMain.java
@@ -3,6 +3,9 @@ package com.bl.snakeandladder;
 public class SnakeAndLadderMain {
 	
 	public static void main(String[] args) {
+		final int LADDER=1;
+		final int SNAKE=2;
+		final int NO_PLAY=3;
 		
 		int position = 0;
 		int dieNum;
@@ -10,7 +13,7 @@ public class SnakeAndLadderMain {
 		
 		System.out.println("Welcome to Snake and Ladder program");
 		
-		while(position<=100) {
+		while(position!=100) {
 			System.out.printf("\nPlayer 1 current position : %d",position);
 			
 			dieNum=(int)Math.floor(Math.random()*((6-1)+1)+1);
@@ -19,20 +22,26 @@ public class SnakeAndLadderMain {
 			
 			option = (int)Math.floor(Math.random()*((3-1)+1)+1);
 			switch(option) {
-			case 1: 
+			case LADDER: 
 				position+=dieNum;
+				if(position>100) {
+					System.out.printf("\nOption Ladder\nPosition exceeds 100. Hence player stays in the same position");
+					position-=dieNum;
+					System.out.printf("\nNew position: %d\n",position);
+					break;
+				}
 				System.out.printf("\nOption Ladder\nPlayer has inclined by %d \nNew position: %d\n",dieNum,position);
 				break;
-			case 2:
+			case SNAKE:
 				position-=dieNum;
 				if(position<0) {
 					position=0;
-					System.out.printf("\nOption Snake\nPlayer has declined by %d \nPosition<0 \nNew position: %d\n",dieNum,position);
+					System.out.printf("\nOption Snake\nPlayer has declined by %d \nPosition<0. Hence position is set to 0 \nNew position: %d\n",dieNum,position);
 					break;
 				}
 				System.out.printf("\nOption Snake \nPlayer has declined by %d \nNew position: %d\n",dieNum,position);
 				break;
-			case 3:
+			case NO_PLAY:
 				System.out.printf("\nOption No Play\nPlayer stays in the same position \nNew Position: %d\n",position);
 			}
 		}
